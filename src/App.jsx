3f76f551e2c126a0c166c11b4f03b328ee5e7b94
Vinay Gandla <24211a05g6@gmail.com>
2026-03-15 16:06:50 +0530

    Added main headings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-
 function App() {
-
-  return  <div>Hello welcome to TO DO APP</div>
+  return (
+    <center>
+      <h1>TO-DO APP</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter the TaskName"></input>
+          </div>
+          <div class="col-4">
+            <input type="date"/>
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">submit</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
